8483ce4130b52558f7de4162f50733b186ffd2d4
manyuet <907878374@qq.com>
2021-03-21 16:50:24 +0800

    week 04 homework

---
+++ Week_04/kmp.js
@@ -0,0 +1,46 @@
+function kmp(source, pattern) {
+    // 计算匹配表
+    let table = new Array(pattern.length).fill(0);
+
+    {
+        let i = 1, j = 0;
+        while(i < pattern.length) {
+            if(pattern[i] === pattern[j]) {
+                ++j;
+                ++i;
+                table[i] = j;
+            } else {
+                if(j > 0) {
+                    j = table[j];
+                } else {
+                    ++i;
+                }
+    
+            }
+        }
+        console.log(table)
+    }
+
+    {
+        let i = 0, j = 0;
+        while(i < source.length) {
+            if(pattern[j] === source[i]) {
+                ++i, ++j;
+            } else {
+                if(j > 0) {
+                    j = table[j]
+                } else {
+                    ++i
+                }
+            }
+            if(j === pattern.length) {
+                return true
+            }
+        }
+        return false;
+    }
+
+
+    // 匹配
+}
+console.log(kmp("hello","ll"))
